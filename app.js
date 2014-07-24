7f936cac8c20852d5cf0f0dd850b3df4c8510a8d
snagaoka <shannonnagaoka@gmail.com>
2014-07-23 22:01:38 -1000

    schema set up complete

--- app.js
+++ app.js
@@ -1,35 +1,75 @@
+// VARIABLES to DEPENDENCIES in the package.json
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database'); // add login
+var jade = require('jade');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-app.get('/', function (req, res) {
-	res.send('hello world!');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+
+// Sets up schema type for mongoose
+
+var Schema = mongoose.Schema;
+  //, ObjectId = Schema.ObjectId;
+
+var toDoSchema = new Schema({
+	title     : String
+  , body      : String
+  //, date      : Date
 });
 
+var toDoModel = mongoose.model('toDo', toDoSchema);
+
+
+// Gets list
+
+app.get('/', function (req, res) {
+	res.send('To-Do List');
+}); // TEST by typing "nodemon app.js in terminal, then go to browser (localhost:3000)"
+
+app.set('views', _direname + '/templates');
+// add body parser
+// add method overwrite
+
+
 // CRUD TaskItems
-
 // INDEX
-// GET /
-// GET /tasks #TODO
+
+// NEW  
+// GET /tasks/new  <-- new form, shows empty form to user
+app.get('/tasks/new', function (req, res){
+	res.send();
+});
+
+
+// CREATE 
+// POST /tasks  <-- allows user to post task item in form field, server receives, then sends to DB 
 
 
 
-// SHOW
-// GET /tasks/:id
+
+// GET / gets ALL tasks 
+// GET /tasks #TODO  <-- server gets data from DB (mongo DB), displays all tasks on redirected page
 
 
 
-// NEW
-// GET /tasks/new
+
+// SHOW / gets ONE task 
+// GET /tasks/:id  <-- server gets data from DB, displays single task on new page
 
 
 
-// CREATE
-// POST /tasks
 
 
 
-// EDIT
-// GET /tasks/:id/edit
+// EDIT / EDIT --> UPDATE similar to NEW --> CREATE
+// GET /tasks/:id/edit 
+
 
 
 
@@ -38,8 +78,12 @@ app.get('/', function (req, res) {
 
 
 
+
+
+
 // DELETE
 // DEL /tasks/:id
+
 
 
 
