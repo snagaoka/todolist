7edbbb0fd56ff9c2bd43055946d18f92a697e3d2
snagaoka <shannonnagaoka@gmail.com>
2014-07-26 18:11:19 -1000

    task counter now working

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
 		$(this).parent().submit();
 		console.log("Hello");
 	});
+	var li = $("li");
+	console.log(li.length);
+	$("h2").append("You have " + li.length + " tasks")
 });
